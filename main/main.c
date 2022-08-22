832714278387a50a46ac805f752f54330445993c
Edouard Levasseur <lewardo23@gmail.com>
2022-08-22 11:06:30 +0100

    updated CMakeLists.txt

--- main/main.c
+++ main/main.c
@@ -1,6 +1,12 @@
 #include "base.h"
 #include "submodule.h"
 
+#include "touch.h"
+
 void app_main() {
+    spct_submodule_init();
+
+    spct_submodule_register(touch);
+    spct_submodule_init_registered();
 
 }
