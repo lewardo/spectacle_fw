92a5762cdb65a29235f15a8ec7054596a63aaa73
lewardo <lewardo23@gmail.com>
2023-02-02 17:34:46 +0000

    started app wrapper

--- components/system/src/app.c
+++ components/system/src/app.c
@@ -14,6 +14,29 @@
 #include "spct_global.h"
 #include "spct_system.h"
 #include "spct_component.h"
+#include "spct_app.h"
 
-#include "system_component.h"
 
+spct_app_handle_t apps[SPCT_APP_MAX_NUM];
+spct_app_handle_t current_app = NULL;
+uint32_t app_num = 0;
+
+spct_ret_t spct_app_install_launcher(spct_app_handle_t launcher) {
+
+    return SPCT_OK;
+};
+
+spct_ret_t spct_app_install(spct_app_handle_t app) {
+
+    return SPCT_OK;
+};
+
+spct_ret_t spct_app_launch(spct_app_handle_t app) {
+
+    return SPCT_OK;
+};
+
+spct_ret_t spct_app_quit() {
+
+    return SPCT_OK;
+};
